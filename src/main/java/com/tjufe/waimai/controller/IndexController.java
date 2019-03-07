5b97fa69b21e8091ac8fcc45442f29881f77d508
zhangyuhang <zhangyuhang@leimingtech.com>
2019-03-07 11:07:16 +0800

    静态资源文件的更新

---
+++ src/main/java/com/tjufe/waimai/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.tjufe.waimai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/login")
+    public String login(){
+        return "/login";
+    }
+    @RequestMapping("/index")
+      public String index(){
+          return "/index";
+      }
+}
